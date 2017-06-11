785670eead65031bb9882e641e7ae51d3440f245
Mark <markchav3z@gmail.com>
2017-06-12 00:11:54 +0800

    add rspec

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,16 +1,20 @@
 class EventsController < ApplicationController
   def index
-    @events = current_user.events
+    @events = events_by_session_user
     render json: @events, each_serializer: EventSerializer
   end
 
   def create
-    @event = current_user.events.build(event_params)
-    @event.end_time = @event.end_time.end_of_day
+    @event = events_by_session_user.build(event_params)
+    @event.set_end_time
     @event.save!
   end
 
   private
+
+    def events_by_session_user
+      Event.belonging_to(current_user)
+    end
 
     def event_params
       params.require(:event).
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ApplicationRecord
   belongs_to :user
   validates :title, :color, :start_time, :end_time, presence: true
+
+  def self.belonging_to(user)
+    Event.where(user: user)
+  end
+
+  def set_end_time
+    self.end_time = self.end_time.end_of_day
+  end
 end
